9fe4e736bcd8e1ca52faade1c13d1c3af85a3e66
홍범선 <qjatjs123123@naver.com>
2025-06-05 22:15:10 +0900

    feat: todo 레이아웃 구현

--- src/pages/calendar/Calendar.tsx
+++ src/pages/calendar/Calendar.tsx
@@ -17,18 +17,67 @@ const Calendar = () => {
       }
       sidebar={<SideBar />}
     >
-      <div className="relative h-full overflow-hidden flex flex-grow flex-shrink basis-auto overflow-y-auto">
-        <TodoTimeCell />
-        <div className="w-full flex relative h-[1152px]">
-          <TodoCellSeparators />
-          <SeparatorBar />
-          <TodoCell/>
-          <TodoCell/>
-          <TodoCell/>
-          <TodoCell/>
-          <TodoCell/>
-          <TodoCell/>
-          <TodoCell/>
+      <div className="overflow-hidden h-full">
+        <div className="w-[100%] h-[81px] flex ">
+          <div className="w-[70px] h-[100%] min-w-[70px] ">
+            <span className="relative top-[64px]">GMT+09</span>
+          </div>
+          <div className="border-b border-b-[#dde3ea] flex w-[calc(100%-81px)]">
+            <div className="w-2 border-r border-[#dde3ea] h-full no-br" />
+
+            <div className="flex relative todo-cell no-br">
+              <div className="separator"></div>
+              <div className="todo-cell no-br flex flex-col items-center justify-center gap-2">
+                <div>일</div>
+                <div>27</div>
+              </div>
+            </div>
+
+            <div className="flex relative todo-cell no-br">
+              <div className="separator"></div>
+              <div className="todo-cell no-br"></div>
+            </div>
+
+            <div className="flex relative todo-cell no-br">
+              <div className="separator"></div>
+              <div className="todo-cell no-br"></div>
+            </div>
+
+            <div className="flex relative todo-cell no-br">
+              <div className="separator"></div>
+              <div className="todo-cell no-br"></div>
+            </div>
+
+            <div className="flex relative todo-cell no-br">
+              <div className="separator"></div>
+              <div className="todo-cell no-br"></div>
+            </div>
+
+            <div className="flex relative todo-cell no-br">
+              <div className="separator"></div>
+              <div className="todo-cell no-br"></div>
+            </div>
+
+            <div className="flex relative todo-cell no-br">
+              <div className="separator"></div>
+              <div className="todo-cell no-br"></div>
+            </div>
+        
+          </div>
+        </div>
+        <div className="relative flex flex-grow flex-shrink basis-auto overflow-y-auto h-full">
+          <TodoTimeCell />
+          <div className="w-full flex relative  h-[1152px]">
+            <TodoCellSeparators />
+            <SeparatorBar />
+            <TodoCell />
+            <TodoCell />
+            <TodoCell />
+            <TodoCell />
+            <TodoCell />
+            <TodoCell />
+            <TodoCell />
+          </div>
         </div>
       </div>
     </Layout>
